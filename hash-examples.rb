a72a7a6360c6ba64cb32a053241b0ee4a171421c
Antonio King <antonio.j.king@gmail.com>
2021-04-20 16:53:02 -0600

    Create car model

---
+++ hash-examples.rb
@@ -0,0 +1,48 @@
+# Hashes are sometimes referred to as objects
+
+# Example using hash rockets
+example_hash = {
+  "name" => "cool object",
+  "favorite_num" => 21,
+  "collection_of_letters" => ['a', 'x', 'p']
+}
+
+# {} symbols denote an hash and are called curly braces.
+# key-value pairs are variables that exist within a hash.
+# keys are the variable names specific to the hash; MUST BE UNIQUE!
+# values are associated to specific keys; can be assigned to any data type.
+# => symbol is referred to as a hash rocket; not necessary if using symbol data types (:name)
+
+# Example using symbol data types
+example_hash = {
+  name: "cool object",
+  favorite_num: 21,
+  collection_of_letters: ['a', 'x', 'p']
+}
+
+# Describe example using the following technical vocabulary:
+1. variable name
+2. assignment operator
+3. curly braces
+4. key-value pairs
+5. hash rocket
+6. double quotes
+
+
+
+# You Do - Modeling Using Hashes
+
+# Add a model for a car
+mazda_cx5 = {
+  stereo: "Apple Carplay",
+  wipers: "auto rain-sensing",
+  front_windows: "tinted",
+  rear_windows: "standard",
+  tow_hitch: "yes",
+  roof_rack: "yes",
+  number_of_seats: 5,
+  cruise_control: "standard",
+  number_of_cup_holders: 8,
+  mpg: 25,
+  is_safe: true
+}
